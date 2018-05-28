8e3bd201e393243c269ab931654da65a4999edd3
Rodrigo Petter <rodrigo-daniel@defensoria.rs.gov.br>
2018-05-28 16:03:41 -0300

    Formata notices e corrige count
    Change-Id: If4060cdb21d5b3e188cb2683bd1f961d9bab85a8

--- data_extraction/vizceralConverter.js
+++ data_extraction/vizceralConverter.js
@@ -6,23 +6,23 @@ function isNode(graph, serviceId) {
 }
 
 function convertToVizceral(rawData) {
-    var data = {
+    let data = {
         name: 'us-west-2',
         renderer: 'region',
         maxVolume: 10000,
         connections: []
     };
 
-    data.nodes = _(rawData).map(function(value, key) {
-        var notices = [];
-        var qtdInstancias = Object.keys(value.instancias).length;
-        var displayName = key;
+    data.nodes = _(rawData).map(function (value, key) {
+        let notices = [];
+        let qtdInstancias = Object.keys(value.instancias).length;
+        let displayName = key;
 
-        if(qtdInstancias > 1) {
+        if (qtdInstancias > 1) {
             displayName = "(" + qtdInstancias + ")" + key;
-            _(value.instancias).each(function(instancia, key) {
+            _(value.instancias).each(function (instancia, key) {
                 notices.push({
-                    title: getSpan(instancia.requestcount, false) + " | " + getSpan(instancia.errorcount, true) + " " + key,
+                    title: instancia.requestcount + ": " + key,
                     severity: (instancia.errorcount > 1) ? 2 : 0
                 });
             });
@@ -34,35 +34,42 @@ function convertToVizceral(rawData) {
         name: 'INTERNET'
     });
 
-    _(rawData).each(function(value, key) {
-        _(value.targets).each(function(targetValue, targetKey) {
-            var connections = {
+    _(rawData).each(function (value, key) {
+        _(value.targets).each(function (targetValue, targetKey) {
+
+            let successCount = targetValue.requestcount - targetValue.errorcount;
+
+            let connections = {
                 source: key, target: targetKey,
                 metrics: {
-                    normal: targetValue.requestcount,
+                    normal: successCount,
                     danger: targetValue.errorcount
                 },
-
                 metadata: {streaming: true}
             };
 
-            var totalRequests = targetValue.requestcount + targetValue.errorcount;
-
-            var severity = (totalRequests >= config.vizceral.alertAtRequestCount) ?
-                config.vizceral.severity.alert : config.vizceral.severity.info;
-
-            if(targetValue.errorcount > 0) {
-                severity = config.vizceral.severity.danger;
-            }
-
-            if(totalRequests >= config.vizceral.infoAtRequestCount || severity === config.vizceral.severity.danger) {
+            if(targetValue.errorcount > 0){
                 connections.notices = [{
-                    title: 'requestcount: ' + targetValue.requestcount + ', errorcount: ' + targetValue.errorcount,
-                    severity: severity
+                    title: 'Success: ' + successCount,
+                    severity: config.vizceral.severity.danger
+                }, {
+                    title: 'Errors: ' + targetValue.errorcount,
+                    severity: config.vizceral.severity.danger
                 }]
+            }else {
+                let severity = (targetValue.requestcount >= config.vizceral.alertAtRequestCount) ?
+                    config.vizceral.severity.alert : config.vizceral.severity.info;
+
+                if (targetValue.requestcount >= config.vizceral.infoAtRequestCount) {
+                    connections.notices = [{
+                        title: 'Total requests: ' + targetValue.requestcount,
+                        severity: severity
+                    }]
+                }
             }
+
             data.connections.push(connections);
-            if(!isNode(data, targetKey)) {
+            if (!isNode(data, targetKey)) {
                 data.nodes.push({name: targetKey})
             }
         })
@@ -79,10 +86,6 @@ function convertToVizceral(rawData) {
     });
 
     return data;
-}
-
-function getSpan(text, erro) {
-    return "<span style='color: " + (erro ? "red" : "blue") + "'>" + text + "</span>";
 }
 
 module.exports = {
